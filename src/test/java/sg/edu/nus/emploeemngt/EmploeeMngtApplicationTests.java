cfab55e826bb76d10ff18c4d0defb36a3d85aff6
Nilesh <nil.y.jadhav@gmail.com>
2023-04-02 08:39:11 +0800

    First Commit

---
+++ src/test/java/sg/edu/nus/emploeemngt/EmploeeMngtApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.emploeemngt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploeeMngtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
